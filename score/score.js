80cb9f8d9bcd5cacee0c76eadc104dbf6302faeb
Jonas Mont <mfernandes.jonas@gmail.com>
2021-03-12 12:40:52 -0300

    score: putting files in the folder

---
+++ score/score.js
@@ -0,0 +1,40 @@
+let score = 100;
+function getScore(score) {
+  let scoreA = score >= 90 && score <= 100;
+  let scoreB = score >= 80 && score <= 89;
+  let scoreC = score >= 70 && score <= 79;
+  let scoreD = score >= 60 && score <= 69;
+  let scoreF = score < 60 && score >= 0;
+
+  let scoreFinal;
+  if (scoreA) {
+    scoreFinal = "A";
+  } else if (scoreB) {
+    scoreFinal = "B";
+  } else if (scoreC) {
+    scoreFinal = "C";
+  } else if (scoreD) {
+    scoreFinal = "D";
+  } else if (scoreF) {
+    scoreFinal = "F";
+  } else {
+    scoreFinal = "Nota inválida.";
+  }
+
+  return scoreFinal;
+}
+
+// coloque sua nota dentro de () exemplo: console.log(getScore(70));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+console.log(getScore(85));
+
